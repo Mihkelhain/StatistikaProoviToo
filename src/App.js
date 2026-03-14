e1dc9078c007121d56a74c77d0b5c6f4e1789098
Mihkelhain <mihkelhain7@gmail.com>
2026-03-15 00:47:04 +0200

    Test kusimused ja kusimuste loogika algus

--- src/App.js
+++ src/App.js
@@ -1,29 +1,60 @@
-import React, { useState } from 'react';
+import React, {use, useState} from 'react';
 import './App.css';
 
 function App() {
-    const [quizState, setGameState] = useState('menu');
+    const [quizStatus, setQuizStatus] = useState('menu');
+    const [punktid, addPoint] = useState(0);
+    const questionsData = [
+        {
+            id: 1,
+            question: "Mis on Eesti pealinn?",
+            options: ["Tartu", "Tallinn", "Narva"],
+            correct: "Tallinn"
+        },
+        {
+            id:2,
+            question: "test2?",
+            options: ["test2", "test3", "test4"],
+            correct: "test2"
+        },
+        {
+            id: 3,
+            question: "Mitu paikset on?",
+            options: ["1", "2", "paris palju"],
+            correct: "1"
+        },
+        {
+            id: 4,
+            question: "Mis on koige parem koht eestis?",
+            options: ["Tartu", "Tallinn", "Tsoglo"],
+            correct: "tsoglo"
+        },
+        ]
 
-    const changeToQuiz = () => {setGameState('quiz');};
-    const changeToEndscreen = () => {setGameState('endScreen');};
-    const changeToMenu = () => {setGameState('menu');};
+    const changeToQuiz = () => {setQuizStatus('quiz');};
+    const nextQuestion = () => {}; //suvaliselt paneb kusimused ette
+    const changeToEndscreen = () => {setQuizStatus('endScreen');};
+    const changeToMenu = () => {setQuizStatus('menu');};
 
+    const questionAnswer = (Answer) => {
+
+    }
   return (
     <div className="App">
       <div className="quizContainer">
-          {quizState === 'menu' && (
+          {quizStatus === 'menu' && (
               <div className="view">
                   <h1>Viktoriin menu</h1>
                   <button className="btn-main" onClick={changeToQuiz}>Alusta viktoriini</button>
               </div>
           )}
-          {quizState === 'quiz' && (
+          {quizStatus === 'quiz' && (
               <div className="view">
                   <h1>Viktoriin ise</h1>
                   <button className="btn-main" onClick={ changeToEndscreen}>Alusta tulemus</button>
               </div>
           )}
-          {quizState === 'endScreen' && (
+          {quizStatus === 'endScreen' && (
               <div className="view">
                   <h1>Viktoriin tulemus</h1>
                   <button className="btn-main" onClick={changeToMenu}>Alusta algus</button>
